4cba62358aac3f31f2d2efbf73213a3024b22db4
deniskondrachuk <deniskondrachuk@gmail.com>
2017-11-01 08:29:57 +0200

    add tooltip directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Map } from 'immutable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+  htmlTemp = '<p>Hello</p>';
   slogan = 'Just movie information';
   title = 'Terminator 1';
   actor = Map({
     firstName: 'Arnold',
     lastName: 'Schwarzenegger',
   });
+
+  getHTML(e) {
+    console.log(e);
+  }
 
   changeActor() {
     this.actor = this.actor.merge({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { TooltipModule } from './tooltip/tooltip';
 
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie.component';
+import { DomHandler } from './tooltip/dom';
 
 Raven.config(
   'https://4c60e9e360f4495aa7527a88ebcd5216@sentry.io/233503'
@@ -19,8 +21,11 @@ export class RavenErrorHandler implements ErrorHandler {
 
 @NgModule({
   declarations: [AppComponent, MovieComponent],
-  imports: [BrowserModule, FormsModule, HttpModule],
-  providers: [{ provide: ErrorHandler, useClass: RavenErrorHandler }],
+  imports: [BrowserModule, FormsModule, HttpModule, TooltipModule],
+  providers: [
+    { provide: ErrorHandler, useClass: RavenErrorHandler },
+    DomHandler,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
